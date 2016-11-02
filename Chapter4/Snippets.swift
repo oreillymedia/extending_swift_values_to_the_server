465895548fee1900ec429f96842856d24977de63
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-11-02 11:09:04 -0400

    Chapter 4 fixes

--- Chapter4/Snippets.swift
+++ Chapter4/Snippets.swift
@@ -1,10 +1,44 @@
-// Code 4-1
+/**
+ Swift Package Manager
+ */
+
+/**
+ 4.3 Importing a Library in your Project
+ */
+
+// Code 4.3.1
+import PackageDescription
+let package = Package(
+    name: "MyProject",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Pipes.git",
+                 majorVersion: 0, minor: 1),
+        ]
+)
+
+// Code 4.3.2
+import Pipes
+
+func sayHello(str: String) -> String {
+    return "Hello, \(str)"
+}
+
+"Alice" |> sayHello |> print
+
+
+/**
+ 4.5 Creating your Own Library
+ */
+
+// Code 4.5.1
 precedencegroup LeftFunctionalApply {
     associativity: left
     higherThan: AssignmentPrecedence
     lowerThan: TernaryPrecedence
 }
 
+
+// Code 4.5.2
 infix operator |> : LeftFunctionalApply
 
 @discardableResult
@@ -14,15 +48,17 @@ public func |> <A, B> (
     return try f(x)
 }
 
-// Code 4-2
+// Code 4.5.3
 import XCTest
 @testable import Pipes
+
 func double(a: Int) -> Int { return 2 * a }
+
 class PipesTests: XCTestCase {
     func testDouble() { XCTAssertEqual(6 |> double, 12) }
 }
 
-// Code 4-3
+// Code 4.5.4
 extension PipesTests {
     static var allTests : [
         ( String, (PipesTests) -> () throws -> Void )
@@ -34,7 +70,11 @@ extension PipesTests {
     }
 }
 
-// Code 4-4
+/**
+ 4.6 Producing More Complex Projects
+ */
+
+// Code 4.6.1
 import PackageDescription
 let package = Package(
     name: "MyWebApp",
@@ -44,7 +84,7 @@ let package = Package(
         Target(name: "C")]
 )
 
-// Code 4-5
+// Code 4.6.2
 let package = Package(
     name: "CSQLite",
     providers: [
